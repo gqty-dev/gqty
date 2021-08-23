d145dcb19f4307925114bd66c9d821b0d30bedaf
Pablo Sáez Parra <pablosaez1995@gmail.com>
2021-08-22 23:12:42 -0400

    minor website improvements

--- internal/website/src/pages/_app.tsx
+++ internal/website/src/pages/_app.tsx
@@ -1,25 +1,26 @@
-import 'remark-admonitions/styles/infima.css';
 import '../../public/style.css';
 
-import { appWithTranslation } from 'next-i18next';
-
 import {
-  extendTheme,
-  theme as chakraTheme,
   Box,
   chakra,
+  extendTheme,
+  theme as chakraTheme,
+  BoxProps,
+  useColorModeValue,
 } from '@chakra-ui/react';
 import { mode } from '@chakra-ui/theme-tools';
+import { ClassNames } from '@emotion/react';
 import {
-  ExtendComponents,
-  handlePushRoute,
+  AppSeoProps,
   CombinedThemeProvider,
   DocsPage,
-  AppSeoProps,
+  ExtendComponents,
+  handlePushRoute,
 } from '@guild-docs/client';
-import { Header, Subheader, Footer } from '@theguild/components';
-import Router from 'next/router';
+import { Footer, Header, Subheader } from '@theguild/components';
+import { appWithTranslation } from 'next-i18next';
 import type { AppProps } from 'next/app';
+import Router from 'next/router';
 
 const BaseAnchor = chakra('a', {
   baseStyle: {
@@ -57,12 +58,36 @@ const a: typeof BaseAnchor = (props) => {
   );
 };
 
+const blockquote = (props: BoxProps) => {
+  const bgColor = useColorModeValue('pink.100', 'pink.700');
+
+  return (
+    <Box
+      borderLeft="5px solid #f380fd"
+      padding="0.8em"
+      borderRadius="5px"
+      mt={4}
+      width="full"
+      bg={bgColor}
+      variant="left-accent"
+      status="info"
+      css={{
+        '> *:first-of-type': {
+          marginTop: 0,
+        },
+      }}
+      {...props}
+    />
+  );
+};
+
 ExtendComponents({
   HelloWorld() {
     return <p>Hello World!</p>;
   },
   Box,
   a,
+  blockquote,
 });
 
 const styles: typeof chakraTheme['styles'] = {
@@ -112,53 +137,66 @@ function AppContent(appProps: AppProps) {
   return (
     <>
       <Header accentColor={accentColor} activeLink="/open-source" themeSwitch />
-      <Subheader
-        activeLink={router.asPath}
-        product={{
-          title: '',
-          description: '',
-          image: {
-            src: '/logo/logo.svg',
-            alt: 'Docs',
-          },
-          onClick: (e) => handlePushRoute('/', e),
+      <ClassNames>
+        {({ css }) => {
+          return (
+            <Subheader
+              activeLink={router.asPath}
+              logoProps={{
+                className: css({
+                  img: {
+                    width: '6rem',
+                  },
+                }),
+              }}
+              product={{
+                title: '',
+                description: '',
+                image: {
+                  src: '/logo/logo.svg',
+                  alt: 'Docs',
+                },
+                onClick: (e) => handlePushRoute('/', e),
+              }}
+              links={[
+                {
+                  children: 'Home',
+                  title: 'Read about Guild Docs',
+                  href: '/',
+                  onClick: (e) => handlePushRoute('/', e),
+                },
+                {
+                  children: 'Examples',
+                  title: 'Check Examples',
+                  href: '/examples',
+                  onClick: (e) => handlePushRoute('/examples', e),
+                },
+                {
+                  children: 'GitHub',
+                  href: 'https://github.com/gqty-dev/gqty',
+                  target: '_blank',
+                  rel: 'noopener norefereer',
+                  title: "Head to the project's GitHub",
+                },
+                {
+                  children: 'Docs',
+                  title: 'Check Documentation',
+                  href: '/docs',
+                  onClick: (e) => handlePushRoute('/docs/getting-started', e),
+                },
+              ]}
+              cta={{
+                children: 'Get Started',
+                title: 'Start using GQty',
+                href: '/docs/getting-started',
+                onClick: (e) => handlePushRoute('/docs/getting-started', e),
+                target: '_blank',
+                rel: 'noopener noreferrer',
+              }}
+            />
+          );
         }}
-        links={[
-          {
-            children: 'Home',
-            title: 'Read about Guild Docs',
-            href: '/',
-            onClick: (e) => handlePushRoute('/', e),
-          },
-          {
-            children: 'Examples',
-            title: 'Check Examples',
-            href: '/examples',
-            onClick: (e) => handlePushRoute('/examples', e),
-          },
-          {
-            children: 'GitHub',
-            href: 'https://github.com/gqty-dev/gqty',
-            target: '_blank',
-            rel: 'noopener norefereer',
-            title: "Head to the project's GitHub",
-          },
-          {
-            children: 'Docs',
-            title: 'Check Documentation',
-            href: '/docs',
-            onClick: (e) => handlePushRoute('/docs/getting-started', e),
-          },
-        ]}
-        cta={{
-          children: 'Get Started',
-          title: 'Start using GQty',
-          href: '/docs/getting-started',
-          onClick: (e) => handlePushRoute('/docs/getting-started', e),
-          target: '_blank',
-          rel: 'noopener noreferrer',
-        }}
-      />
+      </ClassNames>
       {isDocs ? (
         <DocsPage
           appProps={appProps}
