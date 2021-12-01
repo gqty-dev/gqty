1d6ceef719814082c0034a7ccf66e6fc2765e864
Lappihuan <oriano.destefani@gmail.com>
2021-12-01 23:43:21 +0100

    changed test to mount only composable instead of full component

--- packages/vue/test/useQuery.test.ts
+++ packages/vue/test/useQuery.test.ts
@@ -1,40 +1,61 @@
 // import { renderHook } from '@testing-library/vue';
 // import { mountComposition, nextTick } from 'vue-composition-test-utils';
 import { defineComponent, ref } from 'vue';
-import { render, screen, waitFor } from '@testing-library/vue';
+import { waitFor } from '@testing-library/vue';
+import { mount } from 'vue-composable-tester';
 // import flushPromises from 'flush-promises';
 // import { flushPromises } from '@vue/test-utils';
 
 import { createVueTestClient } from './utils';
 
+// test('Basic Non-Suspense', async () => {
+//   const { useQuery } = await createVueTestClient();
+//
+//   const app = defineComponent({
+//     template: `
+//       <span>loading: {{ isLoading }}</span>
+//       <span>hello world {{ query.hello }}!</span>
+//   `,
+//     setup() {
+//       const { query, isLoading } = useQuery();
+//
+//       return { query, isLoading };
+//     },
+//   });
+//
+//   const renderApp = render(app);
+//
+//   await waitFor(() => {
+//     expect(screen.getByText('loading: true')).toBeInTheDocument();
+//   });
+//
+//   expect(screen.getByText('hello world !')).toBeInTheDocument();
+//
+//   await waitFor(() => {
+//     expect(screen.getByText('loading: false')).toBeInTheDocument();
+//   });
+//
+//   expect(screen.getByText('hello world hello world!')).toBeInTheDocument();
+// });
+
 test('Basic Non-Suspense', async () => {
   const { useQuery } = await createVueTestClient();
 
-  const app = defineComponent({
-    template: `
-      <span>loading: {{ isLoading }}</span>
-      <span>hello world {{ query.hello }}!</span>
-  `,
-    setup() {
-      const { query, isLoading } = useQuery();
+  const { result } = mount(() => useQuery());
 
-      return { query, isLoading };
-    },
-  });
-
-  const renderApp = render(app);
+  expect(result.query.value.hello).toBe(undefined);
 
   await waitFor(() => {
-    expect(screen.getByText('loading: true')).toBeInTheDocument();
+    expect(result.isLoading.value).toBe(true);
   });
 
-  expect(screen.getByText('hello world !')).toBeInTheDocument();
+  expect(result.query.value.hello).toBe(undefined);
 
   await waitFor(() => {
-    expect(screen.getByText('loading: false')).toBeInTheDocument();
+    expect(result.isLoading.value).toBe(false);
   });
 
-  expect(screen.getByText('hello world hello world!')).toBeInTheDocument();
+  expect(result.query.value.hello).toBe('hello world');
 });
 
 // test('Basic Suspense', async () => {
