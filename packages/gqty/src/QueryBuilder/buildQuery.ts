868b1c5d2dadc27ad5445590af03f8c025e1c240
Vicary A <vicary.archangel@member.mensa.org>
2022-11-28 00:05:05 +0800

    feat(packages/gqty): add operationName option

--- packages/gqty/src/QueryBuilder/buildQuery.ts
+++ packages/gqty/src/QueryBuilder/buildQuery.ts
@@ -35,10 +35,8 @@ export function createQueryBuilder() {
     selections: Set<Selection> | Selection[],
     {
       type,
-      operationName,
     }: {
       type: 'query' | 'mutation' | 'subscription';
-      operationName?: string;
     },
     normalization?: boolean,
     isGlobalCache?: boolean
