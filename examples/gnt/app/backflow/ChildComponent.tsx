69b6c1f19c68f1a6a70798a7b81fa3f778635121
Vicary A <vicary.archangel@member.mensa.org>
2023-05-15 13:56:00 +0800

    chore: test grommet fetch loop

--- examples/gnt/app/backflow/ChildComponent.tsx
+++ examples/gnt/app/backflow/ChildComponent.tsx
@@ -10,23 +10,25 @@ const ChildComponent: FunctionComponent<Props> = ({ onBackflow }) => {
   const query = useQuery();
 
   return (
-    <>
+    <div className="border rounded-xl p-2 m-2">
       <h1 className="text-xl">Child</h1>
-      <p>
+
+      <p className="mb-2">
         The name is{' '}
-        {query.character({ id: '3' })?.name?.concat('.') ??
+        {query.character({ id: '1' })?.name?.concat('.') ??
           'the Primeagent! No, just kidding...'}
       </p>
 
       <Button
+        size="sm"
         onClick={() => {
           console.debug('a');
           onBackflow?.(query);
         }}
       >
-        Backflow
+        Send query upwards
       </Button>
-    </>
+    </div>
   );
 };
 
--- examples/gnt/app/backflow/page.tsx
+++ examples/gnt/app/backflow/page.tsx
@@ -1,26 +1,75 @@
 'use client';
 
-import { useState } from 'react';
-import { useQuery, type Query } from '~/gqty';
+import { DataTable, Grommet, Layer } from 'grommet';
+import { hpe } from 'grommet-theme-hpe';
+import { useState, type FunctionComponent } from 'react';
+import { useQuery, type Character, type Query } from '~/gqty';
 import ChildComponent from './ChildComponent';
+
+const CharacterList: FunctionComponent<{
+  query: Query;
+  onClick?: (character: Character) => void;
+}> = ({ query, onClick }) => (
+  <DataTable
+    onClickRow={({ datum }) => onClick?.(datum!)}
+    data={query.characters({ filter: { name: 'Alien' } })?.results ?? []}
+    columns={[
+      {
+        property: 'id',
+        header: 'ID',
+        primary: true,
+      },
+      {
+        property: 'name',
+        header: 'Name',
+      },
+    ]}
+  />
+);
+
+const LocationList: FunctionComponent<{ query: Query }> = ({ query }) => (
+  <DataTable
+    onClickRow={() => {}}
+    data={query.locations()?.results ?? []}
+    columns={[
+      {
+        property: 'id',
+        header: 'ID',
+        primary: true,
+      },
+      {
+        property: 'name',
+        header: 'Name',
+      },
+    ]}
+  />
+);
 
 export default function Home() {
   const query = useQuery();
   const [childQuery, setChildQuery] = useState<Query>();
 
   return (
-    <main className="p-5 min-h-screen">
-      <h1 className="text-2xl">Parent</h1>
+    <Grommet full theme={hpe}>
+      <main className="p-5 min-h-screen">
+        <div className="border rounded-xl p-2 m-2">
+          <h1 className="text-2xl">Parent</h1>
 
-      <p>Query using self: {query.character({ id: '1' })?.name}</p>
+          <h2>Query using self scope</h2>
 
-      {childQuery ? (
-        <p>Query using child: {childQuery?.character({ id: '2' })?.name}</p>
-      ) : (
-        <p>Waiting child to pass query upwards...</p>
-      )}
+          <CharacterList query={query} />
 
-      <ChildComponent onBackflow={(query) => setChildQuery(query)} />
-    </main>
+          {childQuery && (
+            <Layer position={'top'} onEsc={() => setChildQuery(undefined)}>
+              <h2>Query using child scope:</h2>
+
+              <LocationList query={childQuery} />
+            </Layer>
+          )}
+        </div>
+
+        <ChildComponent onBackflow={(query) => setChildQuery(query)} />
+      </main>
+    </Grommet>
   );
 }
