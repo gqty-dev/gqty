96e1361c2189ad4aec9ba5e1d297faa1ca9fd7ce
Vicary A <vicary.archangel@member.mensa.org>
2025-02-07 10:00:24 +0000

    chore(deps): update dependency tailwindcss to v4

--- examples/solid/tailwind.config.js
+++
@@ -1,8 +0,0 @@
-/** @type {import('tailwindcss').Config} */
-export default {
-  content: ['./src/**/*.tsx'],
-  theme: {
-    extend: {},
-  },
-  plugins: [require('@tailwindcss/typography')],
-};
